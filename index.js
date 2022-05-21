a2a78ab29856912bbdf386ef24a6545b402341a3
Connor Blake <cblake506@gmail.com>
2022-05-21 16:02:31 -0500

    added questions about employee

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+const inquirer = require('inquirer');
+
+inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of the employee?',
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is the id of the employee?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the email of the employee?',
+    }
+  ])
+  .then((answers) => {
+    const newEmployee = new Employee(answers);
+  });
+
+
 class Employee {
     constructor (name, id, email){
         this.name = name;
