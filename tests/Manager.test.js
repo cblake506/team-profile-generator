e9f1b4a4eb71b4437b929f3b7eca77777c71a4ea
Connor Blake <cblake506@gmail.com>
2022-08-27 16:03:57 -0500

    added manager class and test

---
+++ tests/Manager.test.js
@@ -0,0 +1,30 @@
+const Manager = require('../lib/Manager');
+describe('Manager class', () => {
+    it('Creates an manager', () => {
+        const manager = new Manager('Jared', 10, 'Jared@email.com', 1);
+
+        expect(manager.name).toEqual('Jared');
+        expect(manager.id).toEqual(10);
+        expect(manager.email).toEqual('Jared@email.com');
+        expect(manager.officeNumber).toEqual(1);
+
+    });
+
+    const manager = new Manager('Jared', 10, 'Jared@email.com', 1);
+    it('gets the name', () => {
+        expect(manager.getName()).toEqual('Jared'); 
+    }),
+
+    it('gets the id', () => {
+        expect(manager.getId()).toEqual(10); 
+    }),
+    it('gets the email', () => {
+        expect(manager.getEmail()).toEqual('Jared@email.com'); 
+    }),
+    it('gets the role', () => {
+        expect(manager.getRole()).toEqual('Manager'); 
+    }),
+    it('gets the officeNumber', () => {
+        expect(manager.getGithub()).toEqual(1); 
+    })
+});
